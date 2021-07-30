9f160c26286b626475c0511c652008d87063c4d8
亀島隆平 <kameshimaryuhei@ryuhei.local>
2021-07-31 03:54:58 +0900

    Rspec修正

--- backend/spec/requests/api/v1/passwords_spec.rb
+++ backend/spec/requests/api/v1/passwords_spec.rb
@@ -1,18 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe 'Api::V1::Passwords', type: :request do
-  describe 'GET /update' do
-    let!(:user) { create(:user, password: 'defaultpassword', password_confirmation: 'defaultpassword') }
-    let!(:params) { { auth: { email: user.email, password: 'defaultpassword' } } }
-    let!(:change_password) { { user: { current_password: user.password, password: 'editpassword', password_confirmation: 'editpassword' } } }
-
-    before do
-      post '/api/v1/user_token', params: params
-      put api_v1_passwords_path(change_password)
-    end
-
-    it 'パスワードの変更ができること' do
-      expect(response).to have_http_status(:ok)
-    end
-  end
 end
--- backend/spec/requests/api/v1/posts_spec.rb
+++ backend/spec/requests/api/v1/posts_spec.rb
@@ -13,42 +13,4 @@ RSpec.describe 'Api::V1::Posts', type: :request do
       expect(json['content']).to eq(post.content)
     end
   end
-
-  describe 'GET /update' do
-    let!(:user) { create(:user) }
-    let!(:params) { { auth: { email: user.email, password: user.password } } }
-
-    # ログイン処理
-    before do
-      post '/api/v1/user_token', params: params
-    end
-
-    it '投稿の情報を更新できること' do
-      post = create(:post)
-      update = { post: { title: 'testpost', rate: 3.5, content: 'testcontent' } }
-      put "/api/v1/posts/#{post.id}", params: update
-      json = JSON.parse(response.body)
-      # responseの可否判定
-      expect(response).to have_http_status(:success)
-      expect(json['post']['title']).to eq(update[:post][:title])
-      expect(json['post']['rate']).to eq(update[:post][:rate])
-      expect(json['post']['content']).to eq(update[:post][:content])
-    end
-  end
-
-  describe 'GET /destroy' do
-    let!(:user) { create(:user) }
-    let!(:params) { { auth: { email: user.email, password: user.password } } }
-
-    # ログイン処理
-    before do
-      post '/api/v1/user_token', params: params
-    end
-
-    it '投稿を削除できること' do
-      post = create(:post)
-      delete "/api/v1/posts/#{post.id}"
-      expect(response).to have_http_status(:success)
-    end
-  end
 end
