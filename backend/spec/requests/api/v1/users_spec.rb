e54f4a6b56a353344195532d3343b1515845192c
亀島隆平 <kameshimaryuhei@ryuhei.local>
2021-07-31 04:31:08 +0900

    Rspec修正

--- backend/spec/requests/api/v1/users_spec.rb
+++ backend/spec/requests/api/v1/users_spec.rb
@@ -12,23 +12,4 @@ RSpec.describe 'Api::V1::Users', type: :request do
       expect(json['email']).to eq(user.email)
     end
   end
-
-  describe 'PUT /update' do
-    let!(:user) { create(:user) }
-    let!(:params) { { auth: { email: user.email, password: user.password } } }
-    let!(:update) { { user: { name: 'testuser1', email: 'testuser1@example.com', description: 'よろしくお願いします' } } }
-
-    before do
-      post '/api/v1/user_token', params: params
-    end
-
-    it 'ユーザーの情報を更新' do
-      put "/api/v1/users/#{user.id}", params: update
-      json = JSON.parse(response.body)
-      # responseの可否判定
-      expect(json['name']).to eq(update[:user][:name])
-      expect(json['email']).to eq(update[:user][:email])
-      expect(json['description']).to eq(update[:user][:description])
-    end
-  end
 end
