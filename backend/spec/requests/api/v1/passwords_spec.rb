a3d10b1a8b072364382c0c8f229ae164c6d75090
亀島隆平 <kameshimaryuhei@ryuhei.local>
2021-07-31 04:01:42 +0900

    Rspec修正

--- backend/spec/requests/api/v1/passwords_spec.rb
+++ backend/spec/requests/api/v1/passwords_spec.rb
@@ -1,4 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'Api::V1::Passwords', type: :request do
+  describe 'GET /update' do
+    let!(:user) { create(:user, password: 'defaultpassword', password_confirmation: 'defaultpassword') }
+    let!(:params) { { auth: { email: user.email, password: 'defaultpassword' } } }
+    let!(:change_password) { { user: { current_password: user.password, password: 'editpassword', password_confirmation: 'editpassword' } } }
+
+    before do
+      post '/api/v1/user_token', params: params
+    end
+
+    it 'パスワードの変更ができること' do
+      put api_v1_passwords_path(change_password)
+      expect(response).to have_http_status(:ok)
+    end
+  end
 end
