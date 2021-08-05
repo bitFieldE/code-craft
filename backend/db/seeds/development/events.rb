3c323cc656aacabe466a8c586f3e1bab66a6c74d
亀島隆平 <kameshimaryuhei@ryuhei.local>
2021-08-05 17:51:43 +0900

    シードデータ　本番環境作成

---
+++ backend/db/seeds/development/events.rb
@@ -0,0 +1,141 @@
+events = [
+  {
+    title: 'Ruby勉強会',
+    content: 'ZOOMで開催します！プログラミング学習者がMVC勉強会を開催します！',
+    participant_number: rand(1..15),
+    place: 'Zoom',
+    scheduled_date: Date.today + rand(7..14).day,
+    start_time: '21:30',
+    end_time: '22:40',
+    tags: ['Rails', 'Ruby']
+  },
+  {
+    title: 'Rails6の知見を深めよう',
+    content: 'Ruby開発環境を準備しておくことが望ましいです
+    皆で学びたいちょっとしたテーマありましたらSlackに挙げましょう',
+    participant_number: rand(1..15),
+    place: 'Zoom',
+    scheduled_date: Date.today + rand(7..14).day,
+    start_time: '19:50',
+    end_time: '21:00',
+    tags: ['Rails6', 'Ruby', 'ajax']
+  },
+  {
+    title: 'Hoge会',
+    content: 'お酒でも飲みながら、今後の業界の展望でも語りましょう',
+    participant_number: rand(1..15),
+    place: 'Youtubeかインスタライブ',
+    scheduled_date: Date.today + rand(7..14).day,
+    start_time: '21:00',
+    end_time: '23:00',
+    tags: ['DX', '業界', 'プログラミング']
+  },
+  {
+    title: 'JavaScriptでテトリスを作ろう会',
+    content: '「みんな大好き」 テトリス を作ってみましょう！どなたでも大歓迎です！年齢・レベルは問いません。',
+    participant_number: Date.today + rand(7..14).day,
+    place: 'Zoom',
+    scheduled_date: Date.today + rand(7..14).day,
+    start_time: '20:00',
+    end_time: '23:00',
+    tags: ['Rails', 'Ruby']
+  },
+  {
+    title: 'Vue.js環境構築をしよう会',
+    content: '本講座は、HTML/CSS、JavaScriptを勉強した方向けに、JavaScriptのライブラリである、Vue.jsの基礎を習得して
+      いただく講座です。',
+    participant_number: Date.today + rand(7..14).day,
+    place: 'Whereby',
+    scheduled_date:  Date.today + rand(7..14).day,
+    start_time: '19:50',
+    end_time: '22:00',
+    tags: ['Rails', 'Ruby']
+  },
+  {
+    title: 'もくもく会',
+    content: 'サーバーサイドエンジニアで、PHPを使ってコードを書いています。私自身、駆け出しですので初学者の方や学生の方もご遠慮なく、参加いただけると嬉しいです。',
+    participant_number: rand(1..15),
+    place: 'Whereby',
+    scheduled_date: Date.today + rand(7..14).day,
+    start_time: '21:00',
+    end_time: '23:00',
+    tags: ['Rails', 'Ruby']
+  },
+  {
+    title: '駆け出しエンジニアと語ろう会',
+    content: 'Webクリエイター(Webデザイナー・エンジニア・プログラマー・マーケターetc)で集まり、まったり交流しませんか?',
+    participant_number: rand(1..15),
+    place: '上野',
+    scheduled_date: Date.today + rand(7..14).day,
+    start_time: '15:30',
+    end_time: '18:00',
+    tags: ['Rails', 'Ruby', 'Java', 'Python', 'PHP']
+  },
+  {
+    title: 'Nuxt.js x Rails環境構築をしよう会',
+    content: 'Nuxt.js や Ruby on Rails で開発するための勉強会＆もくもく会です。
+    各々自分が作りたいものを作りましょう！',
+    participant_number: rand(1..15),
+    place: 'Zoom',
+    scheduled_date: Date.today + rand(7..14).day,
+    start_time: '20:30',
+    end_time: '23:00',
+    tags: ['Rails', 'Ruby', 'Vue', 'Nuxt']
+  },
+  {
+    title: 'Rails x Docker環境構築をしよう会',
+    content: 'Ruby や Ruby on Rails で開発するための勉強会＆もくもく会です。
+    各々自分が作りたいものを作りましょう！',
+    participant_number: rand(1..15),
+    place: 'Whereby',
+    scheduled_date: Date.today + rand(7..14).day,
+    start_time: '21:00',
+    end_time: '23:00',
+    tags: ['Rails', 'Ruby', 'Docker']
+  },
+  {
+    title: 'ポートフォリオのレビューを行います',
+    content: '※ポートフォリオが完成して現在転職活動中の方を対象としています。',
+    participant_number: rand(1..10),
+    place: 'Whereby',
+    scheduled_date: Date.today + rand(7..14).day,
+    start_time: '20:30',
+    end_time: '22:00',
+    tags: ['Rails', 'Ruby']
+  }
+]
+
+# イベント掲示板への書き込み
+events_comments = [
+  'よろしくお願いします！',
+  'Rubiistです!よろしくお願いします。',
+  '皆さん、こんにちは。よろしくお願いします。',
+  '初めまして!本日から参加させて頂きます。よろしくお願いします',
+  '今後ともよろしくお願いいたします。'
+]
+
+1.upto(14) do |idx|
+  user = User.find_by(id: idx)
+  post = Post.find_by(id: idx)
+  events.each do |event|
+    event_content =
+    Event.create(
+      user: user,
+      post: post,
+      title: event[:title],
+      content: event[:content],
+      participant_number: event[:participant_number],
+      place: event[:place],
+      scheduled_date: event[:scheduled_date],
+      start_time: event[:start_time],
+      end_time: event[:end_time]
+    )
+    event_content.save_tags(event[:tags])
+  end
+  event_id = rand(1..events.length)
+  user.join_events.create(event_id: event_id)
+  user.event_comments.create(
+    event_id: event_id,
+    content: events_comments[rand(0..events_comments.length - 1)]
+  )
+end
---
+++ backend/db/seeds/development/posts.rb
@@ -0,0 +1,93 @@
+# user0の投稿
+contents = [
+  {
+    title: "RailsでMarkdownのテキストをHTMLに変換する",
+    rate: 5.0,
+    content: "# gem 読み込み\n<br>\nGemfile に以下を追記し、bundle install します。\n\n```ruby\ngem 'redcarpet'\ngem 'rouge'\n```\n\n<br>\n\n# ヘルパー作成\n<br>\napp/helpers/ に markdown_helper.rb を作成します。\n<br>\n\n```ruby\nmodule MarkdownHelper\n  def markdown(text)\n    options = {\n      hard_wrap:       true,\n      space_after_headers: true,\n    }\n\n    extensions = {\n      autolink:           true,\n      no_intra_emphasis:  true,\n      fenced_code_blocks: true,\n    }\n\n    unless @markdown\n      renderer = Redcarpet::Render::HTML.new(options)\n      @markdown = Redcarpet::Markdown.new(renderer, extensions)\n    end\n\n    @markdown.render(text).html_safe\n  end\nend              \n```\n<br>\n\nviewでは以下の記述をすれば表示されます。\n\n```html\n<%= markdown(@post.content) %>\n```\n            ",
+    tags: ['HTML', 'Markdown']
+  },
+  {
+    title: "Nuxt.js使ってみた",
+    rate: 5.0,
+    content: "# vue-cliをインストール\n\n<br>\n\n```shell\nnpm install -g vue-cli\n```\n\n<br>\n\n# プロジェクトの作成\n<br>\n\n```\nvue init nuxt-community/starter-template sample\n```\n\n<br>\n\n```\n? Project name sample\n? Project description Nuxt.js project\n? Author [UserName]\n```\n<br>\n必要なライブラリをインストールします。\n<br>\n\n```\ncd sample\nnpm install\n```\n\n<br>\n開発サーバーを起動。\n<br>\n\n```\nnpm run dev\n```\n\n<br>\nlocalhost:3000でNuxtのサンプルアプリが起動します。\n<br>\n\n```shell\n> sample@1.0.0 dev /Users/takeshi/projects/sample/sample\n> nuxt\n\n  nuxt:build App root: /Users/takeshi/projects/sample/sample +0ms\n  nuxt:build Generating /Users/takeshi/projects/sample/sample/.nuxt files... +1ms\n  nuxt:build Generating files... +49ms\n  nuxt:build Generating routes... +7ms\n  nuxt:build Building files... +21ms\n  nuxt:build Adding webpack middleware... +110ms\nBuild completed in 6.958s\n\n\n\n DONE  Compiled successfully in 6963ms                                                                        21:33:18\n\n\n OPEN  http://localhost:3000\n```\n",
+    tags: ['Nuxt.js', 'Vue']
+  },
+  {
+    title: "axiosを取り入れてみた",
+    rate: 4.0,
+    content: "Nuxt.jsでaxiosを導入するには@nuxtjs/axiosパッケージを追加します。\n<br>\n```bash\nnpm install --save @nuxtjs/axios\n```\n<br>\n@nuxtjs/axiosが有効になるように nuxt.config.jsの修正をする。\n\n```js\nmodule.exports = {\n\t・\n\t・\n\t・\n   /*\n   ** Nuxt.js modules\n   */\n\tmodules: \n\t[\n      \"@nuxtjs/axios\",\n\t],\n\t・\n\t・\n\t・\n};\n```\n\n<br>\n\npage/index.vue ファイルのscriptタグに下記を記載\n\n<br>\n\n```js\n<script>\n\texport default {\n\t\tasync asyncData({ $axios }) {\n\t\t\t// 取得先のURL\n\t\t\tconst url = \"https://qiita.com/api/v2/items\";\n\t\t\t// リクエスト（Get）\n\t\t\tconst response = await $axios.$get(url);\n\t\t\t// 配列で返ってくるのでJSONにして返却\n\t\t\treturn {\n\t\t\t\tposts: response\n\t\t\t};\n\t\t}\n\t};\n</script>\n```\n<br>\n\nQiitaのAPIにリクエストするようにしているので、\nそのまま実行すると、JSON形式でタイトルやURLが返却される。\n\n<br>\n\n```js\n<template>\n\t<div>\n\t\t<ul>\n\t\t \t<li v-for=\"(post, index) in posts\" :key=\"index\">\n\t\t \t\t<a :href=\"'post.url'\" target=\"_blank\" rel=\"noopener noreferrer\">{{ post.title }}</a>\n\t\t\t</li>\n\t\t</ul>\n\t</div>\n</template>\n```\n",
+    tags: ['axios', 'Nuxt.js']
+  },
+  {
+    title: "MySQL/Dockerエラーの対処法",
+    rate: 5.0,
+    content: "**非常に助かりました**\n\n[【MySQL】Unknown MySQL server host 'db' (-2)の対処法【Docker】](https://qiita.com/SyoInoue/items/2ed5b3017c517920ec09)",
+    tags: ['MySQL', 'Docker']
+  },
+  {
+    title: "[Rails]投稿のいいね数のランキング",
+    rate: 4.0,
+    content: "モデルにアソシエーションを定義\n++post.rb++\n```ruby\nclass Post < ApplicationRecord\n    belongs_to :user\n    has_many :likes, dependent: :destroy\n    has_many :liked_users, through: :likes, source: :user\nend\n```\n\n++user.rb++\n```ruby\nclass User < ApplicationRecord\n  has_many :posts,dependent: :destroy\n  has_many :likes, dependent: :destroy\n  has_many :liked_posts, through: :likes, source: :post\nend\n```\n\n++like.rb++\n```ruby\nclass Like < ApplicationRecord\n  belongs_to :post\n  belongs_to :user\nend\n```\n\n++app/controller/posts_controller.rb++\n```ruby\ndef index\n  @posts = Post.includes(:liked_users).sort {|a,b| b.liked_users.size <=> a.liked_users.size}\nend\n```",
+    tags: ['Rails', 'ランキング機能']
+  }
+]
+
+# レビュー記事へのコメント
+comments = [
+  '参考になります！',
+  '私も同じところでつまづいてました',
+  '先人切ってプログラミングしてくれた方々に感謝でしかない',
+  'プログラムを書く前にフローチャートを書くことの大切さが分かったw',
+  '面白かったです。参考になりました。',
+  'すごい！',
+  'プログラミングの入門書の最初の方によくあるやつw',
+  'オブジェクト指向ですね',
+  'とても勉強になりました。
+  ありがとうございました。',
+  'テストは必須かと思いました・・・',
+  '凄いです！勇気をもらいました！！',
+  '実際の案件とその背景を現役エンジニアから話を聞けるのは貴重ですね！',
+  '初心者向けということもあるので、しょうがない部分だとは思いますが圧倒的に大事なのはDB設計。これが本当に大事なんです。',
+  'すんごく良いですね',
+  'ためになり過ぎた。',
+  '初心者の場合は環境構築が最大の難関。',
+  'これは本当に役に立ちました！ありがとうございます！',
+  'これがプロフェッショナル・・・めちゃ参考になりました',
+  '本当に尊敬します。
+  弟子にして下さい'
+]
+
+1.upto(10) do |idx|
+  user = User.find_by(id: idx)
+  contents.each do |content|
+    post = Post.create(
+      user: user,
+      title: content[:title],
+      rate: content[:rate],
+      content: content[:content]
+    )
+    post.save_tags(content[:tags])
+  end
+end
+
+1.upto(14) do |idx|
+  user = User.find_by(id: idx)
+  user.likes.create(post_id: rand(1..50))
+  user.likes.create(post_id: rand(51..100))
+  user.comments.create(
+    post_id: rand(1..50),
+    content: comments[rand(0..comments.length - 1)]
+  )
+  user.comments.create(
+    post_id: rand(1..50),
+    content: comments[rand(0..comments.length - 1)]
+  )
+  user.comments.create(
+    post_id: rand(51..100),
+    content: comments[rand(0..comments.length - 1)]
+  )
+  user.comments.create(
+    post_id: rand(51..100),
+    content: comments[rand(0..comments.length - 1)]
+  )
+end
