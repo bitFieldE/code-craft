57f1e56e3c26a4a030ac5ad66efb0c375a7198bd
亀島隆平 <kameshimaryuhei@ryuhei.local>
2021-08-05 17:39:15 +0900

    ヘルスチェックコントローラー修正

--- backend/app/controllers/api/v1/health_check_controller.rb
+++ backend/app/controllers/api/v1/health_check_controller.rb
@@ -1,5 +1,9 @@
-class Api::V1::HealthCheckController < ApplicationController
-  def index
-    head 200
+module Api
+  module V1
+    class HealthCheckController < ApplicationController
+      def index
+        head 200
+      end
+    end
   end
 end
