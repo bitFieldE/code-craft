bbea91581428bbb4da5f50a856ea7d613837db01
亀島隆平 <kameshimaryuhei@ryuhei.local>
2021-08-09 16:13:56 +0900

    デプロイ修正

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV["FRONTEND_DOMAIN"] || ""
+    origins ENV["FRONTEND_DOMAIN"] || "https://codecraftapp.com"
 
     resource '*',
       headers: :any,
