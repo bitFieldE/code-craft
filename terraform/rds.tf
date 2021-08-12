0a03ea0f490b5f1184ef6b7689abccc50661bb9c
亀島隆平 <kameshimaryuhei@ryuhei.local>
2021-08-13 00:50:33 +0900

    terraform/database.yml 修正

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,11 +1,9 @@
 resource "aws_db_instance" "codecraft-db" {
   allocated_storage       = 20
   instance_class          = "db.t3.micro"
-  identifier              = var.aws_db_identifer
   engine                  = "postgres"
   engine_version          = "13.3"
   storage_type            = "gp2"
-  name                    = var.aws_db_name
   username                = var.aws_db_user
   password                = var.aws_db_password
   backup_retention_period = 7
@@ -21,7 +19,5 @@ resource "aws_db_instance" "codecraft-db" {
 
 }
 
-variable "aws_db_identifer" {}
-variable "aws_db_name" {}
 variable "aws_db_user" {}
 variable "aws_db_password" {}
