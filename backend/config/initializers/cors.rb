5d1131c3be7de7cf4a89b118be2aeddbe1a2b6ae
亀島隆平 <kameshimaryuhei@ryuhei.local>
2021-08-13 15:54:35 +0900

    ホスト名 修正

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://codecraftapp.com/"
+    origins ENV["FRONTEND_DOMAIN"] || ''
 
     resource '*',
       headers: :any,
