b109bf8f0bc731f7ebe4010d88df5c66cd84059b
亀島隆平 <kameshimaryuhei@ryuhei.local>
2021-08-15 14:04:03 +0900

    500 error 調査

--- backend/config/environments/development.rb
+++ backend/config/environments/development.rb
@@ -65,7 +65,5 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  Rails.application.routes.default_url_options[:host] = 'localhost'
-  Rails.application.routes.default_url_options[:port] = 3000
-  config.hosts << "codecraftapp.com"
+  config.action_controller.asset_host = 'http://localhost:3000'
 end
--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -31,10 +31,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :amazon
-  Rails.application.routes.default_url_options[:host] = "codecraftapp.com"
-  Rails.application.routes.default_url_options[:port] = 3000
-  Rails.application.routes.default_url_options[:protocol] = 'https'
+  config.active_storage.service = :local
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
