bfaad8e79c0d160907a47ea5700c7994f3df8a75
亀島隆平 <kameshimaryuhei@ryuhei.local>
2021-08-22 03:58:18 +0900

    active_storage　導入

--- backend/app/controllers/api/v1/top_controller.rb
+++ backend/app/controllers/api/v1/top_controller.rb
@@ -5,7 +5,7 @@ module Api
         posts = Post.joins(:likes).group(:id).order('count(likes.post_id) desc').limit(6)
         events = Event.where('scheduled_date > ? AND scheduled_date <= ?', Date.today, Date.today + 14.day).limit(6)
         render json: {
-          posts: posts.as_json(include: [:user, :liked_users, :tags]),
+          posts: posts.as_json(include: %i[user liked_users tags]),
           events: events.as_json(include: %i[user post tags join_users])
         }
       end
